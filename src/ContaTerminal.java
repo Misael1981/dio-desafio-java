f0db84209cff92b87f1f09668b5ec5bd0bd493d0
Misael Borges <ml_bbd@hotmail.com>
2024-05-12 21:53:04 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner
+
+        // Exibir as msn para o usuário
+
+        // Obter pelo Scanner os valores digitados no terminal
+
+        // Exibir a msn da conta criada
+    }
+}
